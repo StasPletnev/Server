474f5160c7c8db80366fbc071af662d98e740c08
Stas <www.stas011@mail.ru>
2022-10-31 14:31:34 +0300

    Added logic try

---
+++ src/main/java/Server.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(4004)) {
+            try (Socket clientSocket = server.accept();
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+                System.out.println("Сервер запущен!");
+                try {
+                    String word = in.readLine();
+                    System.out.println(word);
+                    out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
+                    out.flush();
+
+                } finally {
+                    clientSocket.close();
+                    in.close();
+                    out.close();
+                }
+            } finally {
+                System.out.println("Сервер закрыт!");
+                server.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
