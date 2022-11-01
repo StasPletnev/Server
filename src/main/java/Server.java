8b2dc8ce3c47bdbc15bc0c1fa62553fe86923d01
Stas <www.stas011@mail.ru>
2022-11-01 15:30:03 +0300

    Small change

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private static int count = 0;
+    private static int countUser = 0;
     public static void main(String[] args) {
         String actualCity = "???";
         int port = 8080;
@@ -16,18 +16,18 @@ public class Server {
                 try (Socket clientSocket = serverSocket.accept();
                      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                    count++;
+                    countUser++;
                     out.println(actualCity);
                     String city = in.readLine();
                     if (city.equals("end")) {
                         out.println("Конец игры");
                         break;
                     }
-                    if (count == 1) {
+                    if (countUser == 1) {
                         actualCity = city;
                         out.println("OK");
                     }
-                    if (count > 1) {
+                    if (countUser > 1) {
                         if (city.charAt(0) == actualCity.charAt(actualCity.length() - 1)) {
                             actualCity = city;
                             out.println("OK");
