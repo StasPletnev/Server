6ddcc84b790570f38f1013a6bbc7b5f39f68b2e3
Stas <www.stas011@mail.ru>
2022-11-01 14:10:24 +0300

    Added logic try

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,11 +2,8 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-    private static BufferedReader in;
-    private static BufferedWriter out;
-
     public static void main(String[] args) {
-        try (Socket clientSocket = new Socket("localhost", 4004)) {
+        try (Socket clientSocket = new Socket("localhost", 8080)) {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                  BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,15 +5,17 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(4004)) {
-            try (Socket clientSocket = server.accept();
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
-                System.out.println("Сервер запущен!");
-                String word = in.readLine();
-                System.out.println(word);
-                out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
-                out.flush();
+        try (ServerSocket server = new ServerSocket(8080)) {
+            while (true){
+                try (Socket clientSocket = server.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                     BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+                    System.out.println("Сервер запущен!");
+                    String word = in.readLine();
+                    System.out.println(word);
+                    out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
+                    out.flush();
+                }
             }
         } catch (IOException e) {
             System.err.println(e);
